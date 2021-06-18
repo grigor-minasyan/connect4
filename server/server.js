52605ad14816f42f33f5418710614730cc14d212
Grigor Minasyan <grigorminasyan1998@gmail.com>
2021-06-18 16:00:16 -0700

    added dotenv

--- server/server.js
+++ server/server.js
@@ -1,110 +1,103 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const devServerRoot = `http://localhost:${8080}`;
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const db_controller = require('./db_controller');
 const cookieSession = require('cookie-session');
-
-
+require('dotenv').config();
 
 // cookieSession config
-app.use(cookieSession({
-  name: 'testSession',
-  maxAge: 24 * 60 * 60 * 1000, // One day in milliseconds
-  keys: ['randomstringhere']
-}));
-
+app.use(
+    cookieSession({
+        name: 'testSession',
+        maxAge: 24 * 60 * 60 * 1000, // One day in milliseconds
+        keys: ['randomstringhere'],
+    })
+);
 
 app.use(passport.initialize()); // Used to initialize passport
 app.use(passport.session()); // Used to persist login sessions
 
-passport.use(new GoogleStrategy(
-  {
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `http://localhost:${port}/auth/google/callback`
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // console.log(profile);
-    // db_controller.findOrCreate(profile.id);
-    return done(null, profile); // passes the profile data to serializeUser
-  }
-));
-
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: `http://localhost:${port}/auth/google/callback`,
+        },
+        function (accessToken, refreshToken, profile, done) {
+            // console.log(profile);
+            // db_controller.findOrCreate(profile.id);
+            return done(null, profile); // passes the profile data to serializeUser
+        }
+    )
+);
 
 passport.serializeUser((user, done) => {
-  db_controller.findOrCreate(user, (err) => done(err, user.id));
+    db_controller.findOrCreate(user, (err) => done(err, user.id));
 });
 
 passport.deserializeUser((id, done) => {
-  console.log(`deserialize got called and id:${id}`);
-  done(null, id);
+    console.log(`deserialize got called and id:${id}`);
+    done(null, id);
 });
 
 isUserAuthenticated = (req, res, next) => {
-  if (req.user) {
-    console.log('req.user');
-    console.log(req.user);
-    return db_controller.getUser(req, res, next);
-    // next();
-  } else {
-    res.locals.userObj = 
-      {
-        googleid: undefined,
-        displayname: 'Anonymous',
-        highscore: 0
-      };
-    next();
-  }
-}
+    if (req.user) {
+        console.log('req.user');
+        console.log(req.user);
+        return db_controller.getUser(req, res, next);
+        // next();
+    } else {
+        res.locals.userObj = {
+            googleid: undefined,
+            displayname: 'Anonymous',
+            highscore: 0,
+        };
+        next();
+    }
+};
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  (req, res) => {
-    res.redirect(devServerRoot);
-    // res.redirect('/');
-  }
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+        res.redirect(devServerRoot);
+        // res.redirect('/');
+    }
 );
 
 // Secret route
 app.get('/secret', isUserAuthenticated, (req, res) => {
-  res.send('You have reached the secret route');
+    res.send('You have reached the secret route');
 });
 
 // Logout route
 app.get('/auth/logout', (req, res) => {
-  req.logout(); 
-  res.redirect(devServerRoot);
+    req.logout();
+    res.redirect(devServerRoot);
 });
 
 app.get('/api/getSignedin', isUserAuthenticated, (req, res) => {
-  res.json(res.locals.userObj);
+    res.json(res.locals.userObj);
 });
 
 app.post('/api/setUserHighScore', db_controller.setHighScore, (req, res) => {
-  res.redirect(devServerRoot);
-})
+    res.redirect(devServerRoot);
+});
 
 app.listen(port, () => {
-  // console.log(process.env.PG_URI);
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-
-
-
-
-
-
-
+    // console.log(process.env.PG_URI);
+    console.log(`Example app listening at http://localhost:${port}`);
+});
 
 /**
  * object returned from google
